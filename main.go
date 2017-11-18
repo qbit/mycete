ca7fd10b76400a73139045b6274e28835e797200
Aaron Bieber <aaron@bolddaemon.com>
2017-11-18 09:11:58 -0700

    split out sending of messages to networks
    log sending status to matrix room

--- main.go
+++ main.go
@@ -16,22 +16,44 @@ import (
 	"github.com/qbit/mycete/protector"
 )
 
+var c goconfig.ConfigMap
+var err error
+
+func notify(client *gomatrix.Client, from, msg string) {
+	log.Printf("%s: %s\n", from, msg)
+	client.SendText(c["matrix"]["room_id"], msg)
+}
+
+func sendTweet(client *twitter.Client, post string) error {
+	_, _, err = client.Statuses.Update(post, nil)
+	return err
+}
+
+func sendToot(client *mastodon.Client, post string) error {
+	_, err := client.PostStatus(context.Background(), &mastodon.Toot{
+		Status: post,
+	})
+	return err
+}
+
 func main() {
-	cfile := flag.String("config", "/etc/mycete.conf", "Configuration file")
+	cfile := flag.String("conf", "/etc/mycete.conf", "Configuration file")
 	flag.Parse()
 
 	protector.Protect("stdio rpath wpath inet dns")
 
-	c, err := goconfig.ParseFile(*cfile)
+	c, err = goconfig.ParseFile(*cfile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	config := oauth1.NewConfig(c["twitter"]["consumer_key"], c["twitter"]["consumer_secret"])
-	token := oauth1.NewToken(c["twitter"]["access_token"], c["twitter"]["access_secret"])
-
-	httpClient := config.Client(oauth1.NoContext, token)
+	cli, _ := gomatrix.NewClient(c["matrix"]["url"], "", "")
+	resp, err := cli.Login(&gomatrix.ReqLogin{
+		Type:     "m.login.password",
+		User:     c["matrix"]["user"],
+		Password: c["matrix"]["password"],
+	})
 
 	mclient := mastodon.NewClient(&mastodon.Config{
 		Server:       c["mastodon"]["server"],
@@ -40,15 +62,10 @@ func main() {
 		AccessToken:  c["mastodon"]["access_token"],
 	})
 
-	// Twitter client
-	client := twitter.NewClient(httpClient)
-
-	cli, _ := gomatrix.NewClient(c["matrix"]["url"], "", "")
-	resp, err := cli.Login(&gomatrix.ReqLogin{
-		Type:     "m.login.password",
-		User:     c["matrix"]["user"],
-		Password: c["matrix"]["password"],
-	})
+	config := oauth1.NewConfig(c["twitter"]["consumer_key"], c["twitter"]["consumer_secret"])
+	token := oauth1.NewToken(c["twitter"]["access_token"], c["twitter"]["access_secret"])
+	httpClient := config.Client(oauth1.NoContext, token)
+	tclient := twitter.NewClient(httpClient)
 
 	if err != nil {
 		fmt.Println(err)
@@ -61,37 +78,47 @@ func main() {
 		panic(err)
 	}
 
+	timeline, err := mclient.GetTimelineHome(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := len(timeline) - 1; i >= 0; i-- {
+		log.Printf("Mastodon: '%s'", timeline[i].Content)
+	}
+
 	syncer := cli.Syncer.(*gomatrix.DefaultSyncer)
 	syncer.OnEventType("m.room.message", func(ev *gomatrix.Event) {
+		if ev.Sender == c["matrix"]["user"] {
+			return
+		}
 		if mtype, ok := ev.MessageType(); ok {
+			log.Println(ev.Sender)
 			switch mtype {
 			case "m.text":
 				if post, ok := ev.Body(); ok {
 					log.Printf("Message: '%s'", post)
 
 					if c["server"]["mastodon"] == "true" {
-						_, err := mclient.PostStatus(context.Background(), &mastodon.Toot{
-							Status: post,
-						})
-
+						err = sendToot(mclient, post)
 						if err != nil {
 							log.Println(err)
 						}
+						notify(cli, "mastodon", "sent toot!")
 					}
 
-					if c["server"]["mastodon"] == "true" {
-						_, _, err = client.Statuses.Update(post, nil)
+					if c["server"]["twitter"] == "true" {
+						err = sendTweet(tclient, post)
 						if err != nil {
 							log.Println(err)
 						}
+						notify(cli, "mastodon", "sent toot!")
 					}
 				}
 			default:
 				fmt.Printf("%s messages are currently not supported", mtype)
 			}
-
 		}
-
 	})
 
 	go func() {
